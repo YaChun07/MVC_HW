12184a2eea2266d0eab077aa2cde5aa5fffee47a
amanda.yang93 <amanda.yang@titansoft.com.sg>
2019-07-12 00:07:35 +0800

    implement account book feature by using partial view, layout

---
+++ MVC_HW/Controllers/AccountBookController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using MVC_HW.Models;
+
+namespace MVC_HW.Controllers
+{
+    public class AccountBookController : Controller
+    {
+        // GET: AccountBook
+        public ActionResult Index()
+        {
+            return View("accountBook", InitAccountBookData());
+        }
+
+        private List<AccountBookViewModel> InitAccountBookData()
+        {
+            var accountBooks = new List<AccountBookViewModel>();
+
+            var random = new Random();
+            var consumeType = new List<string>
+            {
+                "支出","收入"
+            };
+
+            for (var i = 0; i < 100; i++)
+            {
+                accountBooks.Add(new AccountBookViewModel()
+                {
+                    Id = i + 1,
+                    Type = consumeType[random.Next(0, 2)],
+                    Amount = random.Next(1, 3000),
+                    Date = DateTime.Now.AddMinutes(random.Next(1, 3000)).ToString("yyyy-MM-dd")
+                });
+            }
+
+            return accountBooks;
+        }
+    }
+}
---
+++ MVC_HW/Models/AccountBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_HW.Models
+{
+    public class AccountBookViewModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Amount { get; set; }
+    }
+}
