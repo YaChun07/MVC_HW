151ce4a6442989e17c3659b8e0291c4fb1c44ec8
amanda.yang93 <amanda.yang@titansoft.com.sg>
2019-07-19 21:30:30 +0800

    extract class and add accountService

--- MVC_HW/Controllers/AccountBookController.cs
+++ MVC_HW/Controllers/AccountBookController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Web.Mvc;
 using MVC_HW.Models;
-using MVC_HW.Models.Enum;
+using MVC_HW.Models.Service;
 
 namespace MVC_HW.Controllers
 {
     public class AccountBookController : Controller
     {
+        private readonly AccountService _accountService = new AccountService();
+
         // GET: AccountBook
         public ActionResult Index()
         {
@@ -16,19 +17,7 @@ namespace MVC_HW.Controllers
 
         private List<AccountBookViewModel> InitAccountBookData()
         {
-            var accountBooks = new List<AccountBookViewModel>();
-            var accountBookEntity = new AccountBookEntity();
-            var accountBook = accountBookEntity.AccountBook.ToList();
-            foreach (var book in accountBook)
-            {
-                accountBooks.Add(new AccountBookViewModel()
-                {
-                    Amount = book.Amounttt,
-                    Type = ((Category)book.Categoryyy).ToString(),
-                    Date = book.Dateee.ToString("yyyy-MM-dd")
-                });
-            }
-            return accountBooks;
+            return _accountService.GetAccountBooks();
         }
     }
 }
---
+++ MVC_HW/Models/Service/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MVC_HW.Models.Enum;
+
+namespace MVC_HW.Models.Service
+{
+    public class AccountService
+    {
+        public List<AccountBookViewModel> GetAccountBooks()
+        {
+            var accountBooks = new List<AccountBookViewModel>();
+            var accountBook = new AccountBookEntity().AccountBook.ToList();
+
+            foreach (var book in accountBook)
+            {
+                accountBooks.Add(new AccountBookViewModel
+                {
+                    Amount = book.Amounttt,
+                    Type = ((Category)book.Categoryyy).ToString(),
+                    Date = book.Dateee.ToString("yyyy-MM-dd")
+                });
+            }
+
+            return accountBooks;
+        }
+    }
+}
